5d1f0c5b04caf3539c2dfbafe464dddfc7684190
John Wick <muhammadali@cqtechnologies.com>
2019-12-14 11:19:29 +0500

    INITIAL_SETUP

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,21 +1,19 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "../containers/HomeContainer";
 import Login from "../containers/LoginContainer";
 import Signup from "../containers/SignupContainer";
 
-class AppRoutes extends Component {
-  render() {
-    return (
-      <Router>
-        <div className="container">
-          <Switch>
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/signup" component={Signup} />
-          </Switch>
-        </div>
-      </Router>
-    );
-  }
-}
+const AppRoutes = () => (
+  <Router>
+    <div className="container">
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={Signup} />
+      </Switch>
+    </div>
+  </Router>
+);
 
 export default AppRoutes;
